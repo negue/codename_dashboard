7269d4dab86e7dee1f84e212319746a5d678cdfd
negue <eugen.bolz@gmail.com>
2025-03-17 03:04:36 +0100

    how many base configs do you need to change?!

--- _app/immutable/entry/start.CbiNiHqb.js
+++
@@ -1,2 +0,0 @@
-import{l as o,a as r}from"../chunks/D58uAv7x.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.CbiNiHqb.js.map
---
+++ _app/immutable/entry/start.Cx5rKu2w.js
@@ -0,0 +1,2 @@
+import{l as o,a as r}from"../chunks/hnOQlnr3.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.Cx5rKu2w.js.map
