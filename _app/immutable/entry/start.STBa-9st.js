f573263a3e2f465f70a192aa645e15a0ae409327
negue <eugen.bolz@gmail.com>
2025-03-17 02:24:10 +0100

    now with PWA - in the correct path?

---
+++ _app/immutable/entry/start.STBa-9st.js
@@ -0,0 +1,2 @@
+import{l as o,a as r}from"../chunks/C6mMi02X.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.STBa-9st.js.map
--- _app/immutable/entry/start.xgHv2Krj.js
+++
@@ -1,2 +0,0 @@
-import{l as o,a as r}from"../chunks/5wM8y9WC.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.xgHv2Krj.js.map
