a9216e7edee3c240aff63574fd94a35f8f3c2307
negue <eugen.bolz@gmail.com>
2025-03-17 02:43:52 +0100

    now with PWA - in the correct path?!? an eternity lateeer

--- _app/immutable/entry/start.BMzOJrpB.js
+++
@@ -1,2 +0,0 @@
-import{l as o,a as r}from"../chunks/pG2kzf_U.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.BMzOJrpB.js.map
---
+++ _app/immutable/entry/start.D9yd3wKL.js
@@ -0,0 +1,2 @@
+import{l as o,a as r}from"../chunks/BlhLi_6V.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.D9yd3wKL.js.map
